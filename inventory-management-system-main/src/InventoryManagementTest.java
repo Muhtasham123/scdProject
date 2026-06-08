87744f3bc1089b1f925fe0db589a25beca047366
Muhtasham123 <muhtashamfarooqi433@gmail.com>
2026-06-08 20:33:43 +0500

    Added test case for deleting an item from inventory

--- inventory-management-system-main/src/InventoryManagementTest.java
+++ inventory-management-system-main/src/InventoryManagementTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryManagementTest {
-
+	
+	//Insert Test
     @Test
     void testInsertItem() {
         InventoryManagement im = new InventoryManagement();
@@ -13,5 +14,18 @@ public class InventoryManagementTest {
 
         assertTrue(im.inventory.containsKey(1));
         assertEquals("Mouse", im.inventory.get(1).name);
+    }
+    
+    //Delete Test
+    @Test
+    void testDeleteItem() {
+        InventoryManagement im = new InventoryManagement();
+
+        Item item = new Item(2, "Keyboard", "Electronics", 5, 1000.0);
+        im.inventory.put(item.id, item);
+
+        im.inventory.remove(2);
+
+        assertFalse(im.inventory.containsKey(2));
     }
 }
